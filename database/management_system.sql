b38e31812628f11447cedb26ffb6811c3ebd070b
Daniel Nyan <danielcatanb@gmail.com>
2025-05-20 14:00:42 +0800

    Added Database and fixed missing spelling in backend_lumen.

--- database/management_system.sql
+++ database/management_system.sql
@@ -14,11 +14,20 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+-- Dumping data for table management_system.migrations: ~2 rows (approximately)
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2025_05_10_051232_create_users_table', 1),
+	(2, '2025_05_10_052931_create_roles_table', 1);
 
--- Data exporting was unselected.
+-- Dumping data for table management_system.roles: ~2 rows (approximately)
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+	(1, 'Admin'),
+	(2, 'User');
 
--- Data exporting was unselected.
+-- Dumping data for table management_system.users: ~2 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `role_id`, `email`, `password`, `updated_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Admin', 1, 'admin@admin.com', '$2y$12$AR2oRdxgLutP2TLONFfnR.mjAsMucyA.3lM4zcnotIlbBAvuLL5qq', NULL, '2025-05-11 22:07:04', NULL, NULL),
+	(2, 'User', 2, 'user@user.com', '$2y$12$ja.c11eRNGEV27UhOiOmRuEnQ4h1Wfjxh05NPJpVV.pFEdI9oqUky', NULL, '2025-05-12 16:51:49', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
